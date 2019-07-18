cdeea10045d2e5bf8224b0f9be9fba847fabb176
viacheslav-gotsuliak <vgotsuliak@cogniance.com>
2019-07-18 16:44:24 +0300

    gh-pages

---
+++ build/precache-manifest.2caec7a8ee7bc9149528f5779fd5f0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b779f4af8e9e2b4ca5f5efa91a53dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc753c51d9522d4d000",
+    "url": "/static/css/main.360ea435.chunk.css"
+  },
+  {
+    "revision": "20fe98a04a7be48343b0",
+    "url": "/static/js/2.c3142414.chunk.js"
+  },
+  {
+    "revision": "fbc753c51d9522d4d000",
+    "url": "/static/js/main.4b38cb9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "324180605398d90dd60992d5534b2465",
+    "url": "/static/media/viacheslav.gotsuliak.32418060.jpg"
+  }
+]);
